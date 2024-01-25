78787224f5fc157e8cca3f6c5dedae6a7460fdb5
Javier Aleman <javiersettex@gmail.com>
2024-01-24 20:53:43 -0500

    arreglando cagadas

--- src/app/components/buttons/BtnDirection.js
+++ src/app/components/buttons/BtnDirection.js
@@ -22,7 +22,7 @@ const BtnDirection = ({icon,title,dir}) =>{
 
    return(
       <Link href={dir} className={`w-[160px] h-[40px] ${renderColor} flex flex-row rounded-full`}>
-         <div className={`w-[40px] h-[40px] rounded-full bg-slate-50 ${textColor} flex justify-center items-center text-xl text-gray-500`}>
+         <div className={`w-[40px] h-[40px] rounded-full bg-slate-50 ${textColor} flex justify-center items-center text-xl `}>
             {renderIcon}  
          </div>
 			<div className="w-[100px] h-[40px] font-semibold flex justify-center items-center ">
